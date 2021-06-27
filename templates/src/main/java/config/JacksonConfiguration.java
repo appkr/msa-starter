6c1423536a5c60858c75cc9d501aaae19f5ea66b
appkr <juwonkim@me.com>
2021-06-27 15:44:36 +0900

    [templates] removed ObjectMapper bean to avoid stackTrace being printed
    see https://github.com/zalando/problem-spring-web/issues/238

--- templates/src/main/java/config/JacksonConfiguration.java
+++ templates/src/main/java/config/JacksonConfiguration.java
@@ -1,8 +1,5 @@
 package {{packageName}}.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
@@ -21,19 +18,5 @@ public class JacksonConfiguration {
   @Bean
   public JavaTimeModule javaTimeModule() {
     return new JavaTimeModule();
-  }
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.registerModule(javaTimeModule());
-    mapper.disable(
-        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-        DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
-    mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS,
-        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
-        SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-    return mapper;
   }
 }
