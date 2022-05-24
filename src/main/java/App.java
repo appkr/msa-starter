68f3299ed1bb6a02af23aa7e6fc21248153c622e
appkr <juwonkim@me.com>
2022-05-25 08:17:01 +0900

    [starter] removed typescript code; created a java project

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
--- src/model/JavaVersion.ts
+++
@@ -1,4 +0,0 @@
-export enum JavaVersion {
-  JAVA_8 = '1.8',
-  JAVA_11 = '11'
-}
